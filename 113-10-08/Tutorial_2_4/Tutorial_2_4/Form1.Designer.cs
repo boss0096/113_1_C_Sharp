00d4ad56c1e77b97a413fccdea823ac90a33712b
boss0096 <a112060096@mail.shu.edu.tw>
2024-10-08 16:31:27 +0800

    113-10-08

---
+++ 113-10-08/Tutorial_2_4/Tutorial_2_4/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Tutorial_2_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lblshow = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Microsoft JhengHei UI", 20F);
+            label1.Location = new Point(134, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(562, 150);
+            label1.TabIndex = 0;
+            label1.Text = "Click a flag to see the name of the country";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // lblshow
+            // 
+            lblshow.BorderStyle = BorderStyle.Fixed3D;
+            lblshow.Font = new Font("Microsoft JhengHei UI", 15F);
+            lblshow.Location = new Point(251, 289);
+            lblshow.Name = "lblshow";
+            lblshow.Size = new Size(347, 86);
+            lblshow.TabIndex = 1;
+            lblshow.TextAlign = ContentAlignment.MiddleCenter;
+            lblshow.Click += lblshow_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.France;
+            pictureBox2.Location = new Point(378, 186);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(120, 70);
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Finland;
+            pictureBox1.Location = new Point(134, 186);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(120, 70);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = Properties.Resources.Germany;
+            pictureBox3.Image = Properties.Resources.Germany;
+            pictureBox3.Location = new Point(576, 186);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(120, 70);
+            pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            Controls.Add(lblshow);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblshow;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+    }
+}
---
+++ 113-10-08/Tutorial_2_4/Tutorial_2_4/Form1.cs
@@ -0,0 +1,56 @@
+namespace Tutorial_2_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblshow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            lblshow.Text = "Finland";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            lblshow.Text = "France";
+        }
+
+        private void pictureBox3_Click_1(object sender, EventArgs e)
+        {
+            lblshow.Text = "Germany";
+
+        }
+    }
+}
