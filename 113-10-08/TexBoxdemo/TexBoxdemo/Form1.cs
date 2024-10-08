c8a1dcaf205023ff3015ba64a8f584becf7f95e7
boss0096 <a112060096@mail.shu.edu.tw>
2024-10-08 18:01:47 +0800

    113-10-08

---
+++ 113-10-08/TexBoxdemo/TexBoxdemo/Form1.cs
@@ -0,0 +1,10 @@
+namespace TexBoxdemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- HW1/Hw1/Hw1/obj/Debug/net8.0-windows/Hw1.AssemblyInfo.cs
+++ HW1/Hw1/Hw1/obj/Debug/net8.0-windows/Hw1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial_2_5")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Hw1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+814828d392fe8f84e905339bdc857ac27403117b")]
-[assembly: System.Reflection.AssemblyProductAttribute("Tutorial_2_5")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Tutorial_2_5")]
+[assembly: System.Reflection.AssemblyProductAttribute("Hw1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Hw1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
