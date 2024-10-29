69409611c51c1e114fd2fe230a4decdd8b88af26
boss0096 <a112060096@mail.shu.edu.tw>
2024-10-29 18:03:57 +0800

    113-10-29

---
+++ 113-10-29/Tutorial 4-2/Payroll with Overtime/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_with_Overtime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            const decimal BASE_HOURS = 40m;
+            const decimal OT_HULTIPLIER = 1.5m;
+
+            try
+            {
+                decimal hoursWorked, hourlyPayRate, basePay, overtimeHouse, overtimePay, grossPay;
+              
+
+                hoursWorked = decimal.Parse(hoursWorkedTextBox.Text);
+                hourlyPayRate = decimal.Parse(hourlyPayRateTextBox.Text);
+
+                if (hoursWorked > BASE_HOURS)
+                    
+                {
+                    grossPay=(hourlyPayRate * BASE_HOURS) +((hoursWorked - BASE_HOURS)*hourlyPayRate * OT_HULTIPLIER);
+                    
+                
+                    
+                }
+                else
+                {
+                    grossPay = hoursWorked * hourlyPayRate;
+                }
+                grossPayLabel.Text = grossPay.ToString("C");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the TextBoxes and gross pay label.
+            hoursWorkedTextBox.Text = "";
+            hourlyPayRateTextBox.Text = "";
+            grossPayLabel.Text = "";
+
+            // Reset the focus.
+            hoursWorkedTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
---
+++ 113-10-29/Tutorial 4-3/Loan Qualifier/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loan_Qualifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            decimal salary;
+            int yearsOnJob;
+            const decimal MINIMUM_SALART = 1300000m;
+            const int MINIMUM_YEARS_ON_JOB = 2;
+
+            try 
+            {            
+                salary = decimal.Parse(salaryTextBox.Text);
+                yearsOnJob = int.Parse(yearsTextBox.Text);
+
+                if(salary > MINIMUM_SALART)
+                    {
+                    if(yearsOnJob >= MINIMUM_YEARS_ON_JOB)
+                        {
+                        decisionLabel.Text = "You qualify for the loan.";
+                    }
+                    else
+                    {
+                        decisionLabel.Text = "Minimum years at current job not yet";
+                    }
+                }
+                else
+                {
+                    decisionLabel.Text = "Minimum salary requirement not yet";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the TextBoxes and the decisionLabel.
+            salaryTextBox.Text = "";
+            yearsTextBox.Text = "";
+            decisionLabel.Text = "";
+
+            // Reset the focus.
+            salaryTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
