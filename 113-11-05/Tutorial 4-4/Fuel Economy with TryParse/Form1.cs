39ef69eb03005f49fafeb0cc580b461fd3e15d0f
boss0096 <a112060096@mail.shu.edu.tw>
2024-11-05 17:50:05 +0800

    113-11-05

---
+++ 113-11-05/Tutorial 4-4/Fuel Economy with TryParse/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fuel_Economy_with_TryParse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double miles;
+            double gallons;
+            double mpg;
+
+            if (double.TryParse(milesTextBox.Text, out miles))
+            {
+                if(double.TryParse(milesTextBox.Text, out gallons))
+                {
+                 mpg = miles/gallons;
+
+                    mpgLabel.Text = mpg.ToString("n1");        
+                }
+                else
+                {
+                    MessageBox.Show("Invalid input for gallons.");
+                }
+
+        }
+            else
+            {
+            MessageBox.Show("Invvalid input for miles.");
+            }
+           }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
